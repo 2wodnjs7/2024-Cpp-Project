cdf88acdf442641b7bea5ee244ee3deb400593fb
Yoon <168601442+R3BEAt@users.noreply.github.com>
2024-05-12 19:37:53 +0900

    Update scoreboard.cpp

--- src/scoreboard.cpp
+++ src/scoreboard.cpp
@@ -1,8 +1,53 @@
 #include <curses.h>
 #include <iostream>
-#include "scoreboard.h"
 
 using namespace std;
+
+const int scoreRows = 20;
+const int scoreCols = 40;
+
+
+class ScoreBoard {
+private:
+    int currentLength;
+    int maxLength;
+    int growthItems;
+    int poisonItems;
+    int useGate;
+    int missionGoal;
+    bool goalAchieved;
+
+public:
+    ScoreBoard() {
+        currentLength = 0;
+        maxLength = 0;
+        growthItems = 0;
+        poisonItems = 0;
+        useGate = 0;
+        missionGoal = 0;
+        goalAchieved = false;
+    }
+
+    void update(int length, int maxLen, int growth, int poison, int gates, int goal) {
+        currentLength = length;
+        maxLength = maxLen;
+        growthItems = growth;
+        poisonItems = poison;
+        useGate = gates;
+        missionGoal = goal;
+        goalAchieved = (currentLength >= missionGoal);
+    }
+
+    void display() {
+        cout << "Score Board\n";
+        cout << "B: " << currentLength << " / " << maxLength << "\n";
+        cout << "+: " << growthItems << "\n";
+        cout << "-: " << poisonItems << "\n";
+        cout << "G: " << useGate << "\n\n";
+        cout << "Mission\n";
+        cout << "B: " << missionGoal << " (" << (goalAchieved ? "v" : " ") << ")\n";
+    }
+};
 
 int main() {
     initscr();
@@ -20,7 +65,6 @@ int main() {
 
     scoreboard.update(length, maxLen, growth, poison, gates, goal);
     scoreboard.display();
-
 
     endwin();
 
